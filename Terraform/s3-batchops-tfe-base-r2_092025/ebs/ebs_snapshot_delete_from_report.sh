686890f88c37569fd3f1ecae8b1650b5c3383c8c
Tom Gubrud <thomas.gubrud@me.com>
2025-10-18 14:30:22 -0500

    a

--- Terraform/s3-batchops-tfe-base-r2_092025/ebs/ebs_snapshot_delete_from_report.sh
+++ Terraform/s3-batchops-tfe-base-r2_092025/ebs/ebs_snapshot_delete_from_report.sh
@@ -155,7 +155,10 @@ fi
 log "[$(date)] Candidate snapshots to evaluate: $TOTAL"
 
 CNT=0; SUCC=0; FAIL=0; SKIP=0; REFER=0; STATE_SKIP=0; MISSING=0
-while IFS='|' read -r REGION SNAPSHOT SNAP_START CSV_AMI CSV_AMI_STATE CSV_LAUNCH SAFE_FLAG; do
+while IFS= read -r LINE || [[ -n "${LINE:-}" ]]; do
+  [[ -z "$LINE" ]] && continue
+  IFS='|' read -r REGION SNAPSHOT SNAP_START CSV_AMI CSV_AMI_STATE CSV_LAUNCH SAFE_FLAG <<< "$LINE"
+
   # Trim any stray whitespace that could have slipped through (defensive)
   REGION="${REGION#"${REGION%%[![:space:]]*}"}"; REGION="${REGION%"${REGION##*[![:space:]]}"}"
   SNAPSHOT="${SNAPSHOT#"${SNAPSHOT%%[![:space:]]*}"}"; SNAPSHOT="${SNAPSHOT%"${SNAPSHOT##*[![:space:]]}"}"
